2b70ce4673ab2cb9d5b5a402161a38f09d58690b
Renato Augusto <orenato.ames@outlook.com>
2020-11-24 14:45:22 -0400

    test: check resource permission

--- test/service/resource/check-permission.service.test.ts
+++ test/service/resource/check-permission.service.test.ts
@@ -1,21 +1,67 @@
-import { suite, test, timeout } from '@testdeck/jest'
-import { CheckResourcePermissionService } from 'src/service'
+import { suite, test } from '@testdeck/jest'
 import { BaseTest } from 'test/base-test'
+import { FactoryHelper } from 'test/helper'
+import { CheckResourcePermissionService } from 'src/service'
+import { KeycloakClient } from 'src/client'
 
 @suite('Check Resource Permission Service')
 export class CheckResourcePermissionServiceTest extends BaseTest {
-  @test(timeout(10000))
-  async 'Given an user with access in resource then return false'() {
+  @test()
+  async 'Given an user with access in resource then return true'() {
+    const [scope, user] = await Promise.all([
+      new FactoryHelper(super.adminToken).createScope(),
+      new FactoryHelper(super.adminToken).createUser(),
+    ])
+
+    const policy = await new FactoryHelper(super.adminToken).createPolicy([user.id])
+    await new FactoryHelper(super.adminToken).createPermission([scope.id], [policy.id])
+
+    const resource = await new FactoryHelper(super.adminToken).createResourceWithScopes([
+      { id: scope.id, name: scope.name },
+    ])
+
+    const { access_token: accessToken } = await super
+      .get(KeycloakClient)
+      .getToken('skore', 'skore-front', user.username, 'bilu123')
+
     const service = super.get(CheckResourcePermissionService)
 
     const hasAccess = await service.perform(
       'skore',
-      'filesystem',
-      super.commonToken,
-      'movies',
-      'ls',
+      'content',
+      accessToken,
+      resource.name,
+      scope.name,
     )
 
     expect(hasAccess).toBeTruthy()
+  }
+
+  @test()
+  async 'Given an user without access in resource then return false'() {
+    const [scope, user] = await Promise.all([
+      new FactoryHelper(super.adminToken).createScope(),
+      new FactoryHelper(super.adminToken).createUser(),
+    ])
+
+    const resource = await new FactoryHelper(super.adminToken).createResourceWithScopes([
+      { id: scope.id, name: scope.name },
+    ])
+
+    const { access_token: accessToken } = await super
+      .get(KeycloakClient)
+      .getToken('skore', 'skore-front', user.username, 'bilu123')
+
+    const service = super.get(CheckResourcePermissionService)
+
+    const hasAccess = await service.perform(
+      'skore',
+      'content',
+      accessToken,
+      resource.name,
+      scope.name,
+    )
+
+    expect(hasAccess).toBeFalsy()
   }
 }
